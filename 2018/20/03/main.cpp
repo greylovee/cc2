4477c84f450f8bc8bcde4b6a24d364921a7b6edb
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2018-03-20 11:08:22 -0500

    operacion

--- 2018/20/03/main.cpp
+++ 2018/20/03/main.cpp
@@ -7,5 +7,9 @@ int main()
     cout << "Hello world!" << endl;
     cout << " Grettzel " <<endl;
 	//anel se canso de hacer esto
+	//pero sigamos con esto
+	int x= 6;
+	int y=5;
+	int z= x+y;
     return 0;
 }
