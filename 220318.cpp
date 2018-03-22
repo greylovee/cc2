f13cfb67f5c5fbbb45ff69a7f8c9938263b6ab61
ALUMNO-A <ALUMNO-A@UCSP.AULAB>
2018-03-22 09:55:48 -0500

    ejercicio 1

--- 220318.cpp
+++ 220318.cpp
@@ -3,19 +3,47 @@ using namespace std;
 
 int main()
 {
-	int x= 5;
-	int y= 6;
+	/*float x= 5;
+	float y= 6;
 	int suma= x+y;
 	int resta=x-y;
 	int mult= x*y;
-	int div= x/y;
+	float div= x/y;
 	cout<<"el primer numero es   :"<<x<<endl;
 	cout<<"el segundo numero es   :"<<y<<endl;
 	
-	cout<< "la suma es   :"<<suma<<endl;
-	cout<<" la resta es   :"<<resta<<endl;
+	cout<<"la suma es   :"<<suma<<endl;
+	cout<<"la resta es   :"<<resta<<endl;
 	cout<<"la multiplicacion es :"<<mult<<endl;
 	cout<<"la division es   :"<<div<<endl;
+	//////
+	int x,y;
+	cout<<"la suma es"<<x+y<<endl;
+	////////
+	int x,y,z;
+	cout<<"ingrese primer numero  :"; 
+	cin>> x;
+	cout<<"ingrese segundo  numero  :"; 
+	cin >> y;
+	cout<<"ingrese tercer  numero  :";
+	cin >> z;
+	
+	cout<< "la suma es "<<x+y+z<<"\n"<<"la resta es "<<x-y-z<<"\n"<< "la multiplicacion es  :"<<x*y*z<<"\n"<<"la division es :" <<x/y/x<<"\n";
+	 cout << 'a';
+	 cout <<"   "<<'\n';*/
+	//\n salto de linea
+	//\t espacio
+	
+	
+	cout<< "*********\t***\t\t*\t\t*\n*\t*\t**\t***\t**\n*\t*\t*\t*\t*****\t**\n*\t*\t*\t*\t*\t**\n*\t*\t*\t*\t*\t*\n*\t*\t*\t*\t*\t*\n*\t*\t*\t*\t*\t*\n*********\t***\t\t*\t\t*";
+	
+	
+	
+	
+	
+	
+	
+	
 
 	
 	return 0;
