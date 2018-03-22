4d8188a092f4a6811f1e2ead2f486cc88475cd7f
ALUMNO-A <ALUMNO-A@UCSP.AULAB>
2018-03-22 08:56:21 -0500

    ejercicio 1

---
+++ 220318.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x= 5;
+	int y= 6;
+	int suma= x+y;
+	int resta=x-y;
+	int mult= x*y;
+	int div= x/y;
+	cout<<"el primer numero es   :"<<x<<endl;
+	cout<<"el segundo numero es   :"<<y<<endl;
+	
+	cout<< "la suma es   :"<<suma<<endl;
+	cout<<" la resta es   :"<<resta<<endl;
+	cout<<"la multiplicacion es :"<<mult<<endl;
+	cout<<"la division es   :"<<div<<endl;
+
+	
+	return 0;
+}
+
