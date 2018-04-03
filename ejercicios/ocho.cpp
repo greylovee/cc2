1fe7bc2c6e34a84ee3e152e3843dec1732c0a107
greylovee <greylovee@gmail.com>
2018-04-03 10:44:27 -0500

    Update ocho.cpp

--- ejercicios/ocho.cpp
+++ ejercicios/ocho.cpp
@@ -8,18 +8,7 @@ using namespace std;
 
 int main()
 {
-    int numero,p,s,t,c,m;
-    cout<<"ingrese un numero "; cin>>numero;
-    p=numero%10;
-    s=p/10;
-    c=s/10;
-    m=p*100+t*10+c;
-    if (m=numero)
-        cout<<"El Numero es Palindromo\n";
-
-    else
-    cout<<"El Numero no es Palindromo\n";
-
+   
 }
 
 
