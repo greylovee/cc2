a0d70f38dd966adef43053c6d04fc2a39b249644
greylovee <greylovee@gmail.com>
2018-04-26 09:33:47 -0500

    Update punteros.cpp

--- arreglos/punteros.cpp
+++ arreglos/punteros.cpp
@@ -1,24 +1,7 @@
 
 #include <iostream>
 using namespace std;
-/*void conca(char *p,char *q,char *r,char *s,char *t)
-{
-    for (;p<q;p++)
-    {
-       *c=*p;
-       c++
-    }
-    for (;r<s;r++)
-    {
-       *c=*r;
-       c++;
-    }
-}*/
-/*void imprimir(char *t,char *w)
-{
-    for (;t<w;t++)
-        cout <<t;
-}*/
+
 int main()
 {
     char a[]="Grettzel";
@@ -34,9 +17,7 @@ int main()
     char *s=&b[tam2];
     char *t=c;
     char *w=&c[tam3];
-    //conca (p,q,r,s,t);
-     //t=c;
-    //imprimir (t,w);
+    
     for (;p<q;p++)
     {
        *t=*p;
@@ -48,7 +29,7 @@ int main()
        t++;
     }
 
-    //for (t=c;t<w;t++)
+   
     cout <<c;
 
     return 0;
